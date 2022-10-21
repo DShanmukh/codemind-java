30bc897e856e333d0b8f66d7b35e7daa8dd1fe9f
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-10-21 19:05:16 +0530

    import java.util.Scanner; class Odd {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int a[],i,min=0;         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]%2==0)             {                 min=a[i];             }         }         System.out.println(min);
        }
    }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Odd
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int a[],i,min=0;
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                min=a[i];
+            }
+        }
+        System.out.println(min);
+        
+    }
+}
