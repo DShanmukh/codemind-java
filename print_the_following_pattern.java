9e5e93d965585c68f74bbb5bccbc36f3a7c3b68b
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-12-12 15:46:01 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int i,j;         for(i=0;i<n;i++)         {         for(j=n;j>0;j--)         {             System.out.printf("%d ",j);         }         System.out.printf(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
-class Naruto
+class Main
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,i,j;
+        int n;
         n=sc.nextInt();
+        int i,j;
         for(i=0;i<n;i++)
         {
-            for(j=0;j<n;j++)
-            {
-                if(i==j)
-                {
-                    System.out.printf("0");
-                }
-                else
-                {
-                    System.out.printf("x");
-                }
-            }
-            System.out.printf("
+        for(j=n;j>0;j--)
+        {
+            System.out.printf("%d ",j);
+        }
+        System.out.printf("
 ");
         }
-        
     }
 }
